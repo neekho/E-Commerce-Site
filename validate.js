590442eda2a6328394a54da5e22962302e50a04d
Nikho <remulla1920945@mkt.ceu.edu.ph>
2021-10-25 09:20:22 +0800

    first commit

---
+++ validate.js
@@ -0,0 +1,60 @@
+function checkPass() {
+    //Store the password field objects into variables ...
+    var pass1 = document.getElementById('pass1');
+    var pass2 = document.getElementById('pass2');
+    //Store the Confimation Message Object ...
+    var message = document.getElementById('confirmMessage');
+    //Set the colors we will be using ...
+    var goodColor = "#66cc66";
+    var badColor = "#ff6666";
+    //Compare the values in the password field 
+    //and the confirmation field
+    if (pass1.value == pass2.value){
+        //The passwords match. 
+        //Set the color to the good color and inform
+        //the user that they have entered the correct password 
+        pass2.style.backgroundColor = goodColor;
+        message.style.color = goodColor;
+        message.innerHTML = "Passwords Match"
+    }
+    
+    else {
+        //The passwords do not match.
+        //Set the color to the bad color and
+        //notify the user.
+        pass2.style.backgroundColor = badColor;
+        message.style.color = badColor;
+        message.innerHTML = "Passwords Do Not Match!"
+    }
+} 
+
+
+//validate phone number...
+function validatephone(phone) {
+    var maintainplus = '';
+    var numval = phone.value
+    if (numval.charAt(0)=='+' ) {
+        var maintainplus = '';
+    }
+
+    curphonevar = numval.replace(/[\\A-Za-z!"£$%^&\,*+_={};:'@#~,.Š\/<>?|`¬\]\[]/g,'');
+    phone.value = maintainplus + curphonevar;
+    var maintainplus = '';
+    phone.focus;
+}
+
+function Validate(txt) {
+    // validates text only
+    txt.value = txt.value.replace(/[^a-zA-Z-'\n\r.]+/g, '');
+}
+
+
+function email_validate(email) {
+    // email address
+    var regMail = /^([_a-zA-Z0-9-]+)(\.[_a-zA-Z0-9-]+)*@([a-zA-Z0-9-]+\.)+([a-zA-Z]{2,3})$/;
+
+    if (!regMail.test(email)) 
+        document.getElementById("status").innerHTML    = "<span class='warning'>Invalid Email</span>";
+    
+
+}
